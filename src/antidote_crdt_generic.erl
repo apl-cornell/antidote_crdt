0d848e8282d7f4f3f1a293d703c8ee42af199ae6
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2019-10-28 02:56:10 -0400

    Added correct messaging to crdt

--- src/antidote_crdt_generic.erl
+++ src/antidote_crdt_generic.erl
@@ -1,96 +1,108 @@
 -module(antidote_crdt_generic).
 
 %% Callbacks
--export([ new/0,
-          value/1,
-          downstream/2,
-          update/2,
-          equal/2,
-          to_binary/1,
-          from_binary/1,
-          is_operation/1,
-          require_state_downstream/1
-        ]).
+-export([downstream/2, equal/2, from_binary/1,
+	 is_operation/1, new/0, require_state_downstream/1,
+	 to_binary/1, update/2, value/1]).
 
 -behaviour(antidote_crdt).
 
--export_type([antidote_crdt_generic/0, binary_antidote_crdt_generic/0, antidote_crdt_generic_op/0]).
+-export_type([antidote_crdt_generic/0,
+	      binary_antidote_crdt_generic/0,
+	      antidote_crdt_generic_op/0]).
 
 -opaque antidote_crdt_generic() :: objectid().
 
 -type binary_antidote_crdt_generic() :: binary().
+
 %% A binary that from_binary/1 will operate on.
 
--type antidote_crdt_generic_op() ::
-      {invoke, data()}
-    | {invoke_all, [data()]}.
+-type antidote_crdt_generic_op() :: {invoke, data()} |
+				    {invoke_all, [data()]}.
 
 %% element, AddTime, RemoveTime
 -type downstream_op() :: [data()].
 
 -type objectid() :: binary().
+
 -type data() :: {binary(), non_neg_integer()}.
 
 -spec new() -> antidote_crdt_generic().
+
 new() -> unique().
 
 %% add a value call to server
 % will need to set up something on the java side for reads
--spec value(antidote_crdt_generic()) -> [term()].
+-spec value(antidote_crdt_generic()) -> objectid().
+
 value(Generic) -> Generic.
 
--spec downstream(antidote_crdt_generic_op(), antidote_crdt_generic()) -> {ok, downstream_op()}.
-downstream({invoke, Elem}, Generic) ->
-  downstream({invoke_all, [Elem]}, Generic);
-downstream({invoke_all, Elems}, Generic) ->
-  CreateDownstream = fun(Elem, Id) ->
-      {Elem, Id}
-  end,
-  DownstreamOps = create_downstreams(CreateDownstream, lists:usort(Elems), Generic, []),
-  {ok, lists:reverse(DownstreamOps)}.
+-spec downstream(antidote_crdt_generic_op(),
+		 antidote_crdt_generic()) -> {ok, downstream_op()}.
 
-create_downstreams(_CreateDownstream, [], _Generic, DownstreamOps) ->
-  DownstreamOps;
-create_downstreams(_CreateDownstream, _Elems, [], DownstreamOps) ->
-  DownstreamOps;
+downstream({invoke, Elem}, Generic) ->
+    downstream({invoke_all, [Elem]}, Generic);
+downstream({invoke_all, Elems}, Generic) ->
+    CreateDownstream = fun (Elem, Id) -> {Elem, Id} end,
+    DownstreamOps = create_downstreams(CreateDownstream,
+				       lists:usort(Elems), Generic, []),
+    {ok, lists:reverse(DownstreamOps)}.
+
+create_downstreams(_CreateDownstream, [], _Generic,
+		   DownstreamOps) ->
+    DownstreamOps;
+create_downstreams(_CreateDownstream, _Elems, [],
+		   DownstreamOps) ->
+    DownstreamOps;
 %% will need to do time ordering
-create_downstreams(CreateDownstream, [Elem1|ElemsRest], Generic, DownstreamOps) ->
-  DownstreamOp = CreateDownstream(Elem1, Generic),
-  create_downstreams(CreateDownstream, ElemsRest, Generic, [DownstreamOp|DownstreamOps]).
+create_downstreams(CreateDownstream,
+		   [Elem1 | ElemsRest], Generic, DownstreamOps) ->
+    DownstreamOp = CreateDownstream(Elem1, Generic),
+    create_downstreams(CreateDownstream, ElemsRest, Generic,
+		       [DownstreamOp | DownstreamOps]).
 
 %% @doc generate a unique identifier (best-effort).
-unique() ->
-    crypto:strong_rand_bytes(20).
+unique() -> crypto:strong_rand_bytes(20).
 
--spec update(downstream_op(), antidote_crdt_generic()) -> {ok, antidote_crdt_generic()}.
-  update(DownstreamOp, Generic) ->
+-spec update(downstream_op(),
+	     antidote_crdt_generic()) -> {ok,
+					  antidote_crdt_generic()}.
+
+update(DownstreamOp, Generic) ->
     {ok, apply_downstreams(DownstreamOp, Generic)}.
 
 %% @private apply a list of downstream ops to a given orset
 apply_downstreams([], Generic) -> Generic;
 %% When I do time ordering in create_downstream, will not longer need time here
-apply_downstreams([{Binary1, _Time}|OpsRest], Generic) ->
-      [apply_downstream(Binary1, Generic) | apply_downstreams(OpsRest, Generic)].
+apply_downstreams([{Binary1, _Time} | OpsRest],
+		  Generic) ->
+    [apply_downstream(Binary1, Generic)
+     | apply_downstreams(OpsRest, Generic)].
 
 %% @private create an orddict entry from a downstream op
 apply_downstream(Binary, Generic) ->
-  % send and recieve message
-  %% need to have set up node on initialization with % erl -name JavaNode
-  net_adm:ping(foo@domain), % creates association if not already there
-  PID = spawn(?MODULE, java-server, []),
-  PID ! {Generic, Binary}, % sends message
-  R = receive
-      _M -> {Generic}
-  after 5000 -> {error,"no answer!"}
-  end,
-  {R}.
+    % send and recieve message
+    net_kernel:connect_node('JavaNode@dhcp-gs-1201'), % creates association if not already there
+    {javamailbox, 'JavaNode@dhcp-gs-1201'} !
+      {self(),
+       {Generic, Binary}}, % sends the generic function
+    R = receive
+	  error ->
+	      throw("Oh no, an error has occurred");
+	  _M -> {Generic}
+	  after 5000 -> {"no answer!"}
+	end,
+    R.
 
 %% Later work in a better equality
--spec equal(antidote_crdt_generic(), antidote_crdt_generic()) -> boolean().
-equal(GenericA, GenericB) ->
-    GenericA == GenericB.
+-spec equal(antidote_crdt_generic(),
+	    antidote_crdt_generic()) -> boolean().
 
--spec to_binary(antidote_crdt_generic()) -> binary_antidote_crdt_generic().
+equal(GenericA, GenericB) -> GenericA == GenericB.
+
+-spec
+     to_binary(antidote_crdt_generic()) -> binary_antidote_crdt_generic().
+
 to_binary(Binary) -> Binary.
 
 from_binary(<<Bin/binary>>) -> {ok, Bin}.
