0314b1debf3c3b2b8ba289132792d9bb561cbe5a
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2019-11-12 13:08:14 -0500

    Generic file read, bug fixes, and tests

--- src/antidote_crdt_generic.erl
+++ src/antidote_crdt_generic.erl
@@ -1,13 +1,26 @@
 -module(antidote_crdt_generic).
 
--define(BACKEND, 'JavaNode@Abookwithnopages').
-
 %% Callbacks
 -export([downstream/2, equal/2, from_binary/1,
 	 is_operation/1, new/0, require_state_downstream/1,
 	 to_binary/1, update/2, value/1]).
 
+-export_type([antidote_crdt_generic/0,
+	      binary_antidote_crdt_generic/0,
+	      antidote_crdt_generic_op/0]).
+
 -behaviour(antidote_crdt).
+
+-define(HOSTFILE, "host_config.txt").
+
+gethost(FILE) ->
+    % [read] is a list of modes that the file should be opened as
+    {ok, Device} = file:open(FILE, [read]),
+    try io:get_line(Device, "") after
+      file:close(Device)
+    end.
+
+-define(BACKEND, list_to_atom(gethost(?HOSTFILE))).
 
 -ifdef(TEST).
 
@@ -15,20 +28,13 @@
 
 -endif.
 
--export_type([antidote_crdt_generic/0,
-	      binary_antidote_crdt_generic/0,
-	      antidote_crdt_generic_op/0]).
-
 -opaque antidote_crdt_generic() :: objectid().
 
 -type binary_antidote_crdt_generic() :: binary().
 
-%% A binary that from_binary/1 will operate on.
-
 -type antidote_crdt_generic_op() :: {invoke, data()} |
 				    {invoke_all, [data()]}.
 
-%% element, AddTime, RemoveTime
 -type downstream_op() :: [data()].
 
 -type objectid() :: binary().
@@ -49,7 +55,7 @@ value(Generic) ->
     R = receive
 	  error -> throw("Oh no, an error has occurred");
 	  M -> M
-	  after 5000 -> {"no answer!"}
+	  after 5000 -> io:fwrite("No answer~n"), {"no answer!"}
 	end,
     R.
 
@@ -60,7 +66,7 @@ downstream({invoke, Elem}, Generic) ->
     downstream({invoke_all, [Elem]}, Generic);
 %% rethink this
 downstream({invoke_all, Elems}, _Generic) ->
-    {ok, lists:reverse(Elems)}.
+    {ok, Elems}.
 
 unique() -> crypto:strong_rand_bytes(20).
 
@@ -68,8 +74,15 @@ unique() -> crypto:strong_rand_bytes(20).
 	     antidote_crdt_generic()) -> {ok,
 					  antidote_crdt_generic()}.
 
+last_elm([]) -> [];
+last_elm([A]) -> A;
+last_elm([_ | A]) -> last_elm(A);
+last_elm(A) -> A.
+
+% Want only the last genericid in list, maybe just use the Generic variable instead?
 update(DownstreamOp, Generic) ->
-    {ok, apply_downstreams(DownstreamOp, Generic)}.
+    {ok,
+     last_elm(apply_downstreams(DownstreamOp, Generic))}.
 
 apply_downstreams([], Generic) -> Generic;
 apply_downstreams([Binary1 | OpsRest], Generic) ->
@@ -106,9 +119,99 @@ is_operation({invoke, _Elem}) -> true;
 is_operation({invoke_all, L}) when is_list(L) -> true;
 is_operation(_) -> false.
 
-require_state_downstream(_) -> true.
+require_state_downstream(_) -> false.
 
 %% TESTS
 -ifdef(TEST).
+
+prepare_and_effect(Op, Generic) ->
+    {ok, Downstream} = downstream(Op, Generic),
+    update(Downstream, Generic).
+
+update_invoke_test() ->
+    net_kernel:start([erlnode, shortnames]),
+    Counter_object = <<172, 237, 0, 5, 115, 114, 0, 12, 109,
+		       97, 105, 110, 46, 67, 111, 117, 110, 116, 101, 114, 0,
+		       0, 0, 0, 0, 0, 0, 1, 2, 0, 1, 73, 0, 5, 99, 111, 117,
+		       110, 116, 120, 112, 0, 0, 0, 0>>,
+    Increment_1_object = <<172, 237, 0, 5, 115, 114, 0, 20,
+			   109, 97, 105, 110, 46, 71, 101, 110, 101, 114, 105,
+			   99, 70, 117, 110, 99, 116, 105, 111, 110, 0, 0, 0, 0,
+			   0, 0, 0, 1, 2, 0, 2, 76, 0, 8, 65, 114, 103, 117,
+			   109, 101, 110, 116, 116, 0, 18, 76, 106, 97, 118, 97,
+			   47, 108, 97, 110, 103, 47, 79, 98, 106, 101, 99, 116,
+			   59, 76, 0, 12, 70, 117, 110, 99, 116, 105, 111, 110,
+			   78, 97, 109, 101, 116, 0, 18, 76, 106, 97, 118, 97,
+			   47, 108, 97, 110, 103, 47, 83, 116, 114, 105, 110,
+			   103, 59, 120, 112, 115, 114, 0, 17, 106, 97, 118, 97,
+			   46, 108, 97, 110, 103, 46, 73, 110, 116, 101, 103,
+			   101, 114, 18, 226, 160, 164, 247, 129, 135, 56, 2, 0,
+			   1, 73, 0, 5, 118, 97, 108, 117, 101, 120, 114, 0, 16,
+			   106, 97, 118, 97, 46, 108, 97, 110, 103, 46, 78, 117,
+			   109, 98, 101, 114, 134, 172, 149, 29, 11, 148, 224,
+			   139, 2, 0, 0, 120, 112, 0, 0, 0, 1, 116, 0, 9, 105,
+			   110, 99, 114, 101, 109, 101, 110, 116>>,
+    Decrement_1_object = <<172, 237, 0, 5, 115, 114, 0, 20,
+			   109, 97, 105, 110, 46, 71, 101, 110, 101, 114, 105,
+			   99, 70, 117, 110, 99, 116, 105, 111, 110, 0, 0, 0, 0,
+			   0, 0, 0, 1, 2, 0, 2, 76, 0, 8, 65, 114, 103, 117,
+			   109, 101, 110, 116, 116, 0, 18, 76, 106, 97, 118, 97,
+			   47, 108, 97, 110, 103, 47, 79, 98, 106, 101, 99, 116,
+			   59, 76, 0, 12, 70, 117, 110, 99, 116, 105, 111, 110,
+			   78, 97, 109, 101, 116, 0, 18, 76, 106, 97, 118, 97,
+			   47, 108, 97, 110, 103, 47, 83, 116, 114, 105, 110,
+			   103, 59, 120, 112, 115, 114, 0, 17, 106, 97, 118, 97,
+			   46, 108, 97, 110, 103, 46, 73, 110, 116, 101, 103,
+			   101, 114, 18, 226, 160, 164, 247, 129, 135, 56, 2, 0,
+			   1, 73, 0, 5, 118, 97, 108, 117, 101, 120, 114, 0, 16,
+			   106, 97, 118, 97, 46, 108, 97, 110, 103, 46, 78, 117,
+			   109, 98, 101, 114, 134, 172, 149, 29, 11, 148, 224,
+			   139, 2, 0, 0, 120, 112, 0, 0, 0, 1, 116, 0, 9, 100,
+			   101, 99, 114, 101, 109, 101, 110, 116>>,
+    Counter_value_0 = <<172, 237, 0, 5, 115, 114, 0, 17,
+			106, 97, 118, 97, 46, 108, 97, 110, 103, 46, 73, 110,
+			116, 101, 103, 101, 114, 18, 226, 160, 164, 247, 129,
+			135, 56, 2, 0, 1, 73, 0, 5, 118, 97, 108, 117, 101, 120,
+			114, 0, 16, 106, 97, 118, 97, 46, 108, 97, 110, 103, 46,
+			78, 117, 109, 98, 101, 114, 134, 172, 149, 29, 11, 148,
+			224, 139, 2, 0, 0, 120, 112, 0, 0, 0, 0>>,
+    Counter_value_1 = <<172, 237, 0, 5, 115, 114, 0, 17,
+			106, 97, 118, 97, 46, 108, 97, 110, 103, 46, 73, 110,
+			116, 101, 103, 101, 114, 18, 226, 160, 164, 247, 129,
+			135, 56, 2, 0, 1, 73, 0, 5, 118, 97, 108, 117, 101, 120,
+			114, 0, 16, 106, 97, 118, 97, 46, 108, 97, 110, 103, 46,
+			78, 117, 109, 98, 101, 114, 134, 172, 149, 29, 11, 148,
+			224, 139, 2, 0, 0, 120, 112, 0, 0, 0, 1>>,
+    Generic = new(),
+    {ok, Generic1} = prepare_and_effect({invoke,
+					 Counter_object},
+					Generic),
+    ?assertEqual(Counter_value_0, (value(Generic1))),
+    {ok, Generic2} = prepare_and_effect({invoke,
+					 Increment_1_object},
+					Generic1),
+    ?assertEqual(Counter_value_1, (value(Generic2))),
+    {ok, Generic3} = prepare_and_effect({invoke,
+					 Decrement_1_object},
+					Generic2),
+    ?assertEqual(Counter_value_0, (value(Generic3))).
+
+equal_test() ->
+    Generic1 = new(),
+    Generic2 = new(),
+    ?assertNot((equal(Generic1, Generic2))),
+    ?assert((equal(Generic1, Generic1))).
+
+binary_test() ->
+    Generic = new(),
+    B = to_binary(Generic),
+    ?assertEqual({ok, Generic}, (from_binary(B))).
+
+is_operation_test() ->
+    ?assertEqual(true, (is_operation({invoke, {<<0>>}}))),
+    %?assertEqual(true,
+    %(is_operation({invoke_all, {[<<0>>]}}))),
+    ?assertEqual(false,
+		 (is_operation({anything, [1, 2, 3]}))).
 
 -endif.
